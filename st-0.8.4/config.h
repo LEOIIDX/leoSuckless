cecbc8d46bb88a1bb86b1771dac350bfa677796c
Joshua Stevens <chathoo34@gmail.com>
2021-11-24 16:59:42 -0600

    light gray

--- st-0.8.4/config.h
+++ st-0.8.4/config.h
@@ -99,7 +99,7 @@ float alpha = 0.6;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"#222222",
+	"#333333",
 	"#d24d44",
 	"#73d0a3",
 	"#ffe27b",
